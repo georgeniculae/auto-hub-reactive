7fbf610d446ca01fd2a6f9de9a78b6e3888d2aac
George Niculae <george.niculae20@yahoo.com>
2025-05-31 01:36:19 +0300

    Update

--- auto-hub-reactive-agency/src/main/java/com/autohubreactive/agency/migration/DatabaseCollectionCreator.java
+++ auto-hub-reactive-agency/src/main/java/com/autohubreactive/agency/migration/DatabaseCollectionCreator.java
@@ -14,31 +14,31 @@ public class DatabaseCollectionCreator {
 
     public static List<RentalOffice> getRentalOffices() {
         return List.of(
-                JsonUtil.getResourceAsJson("/migration/rental_office/BucurestiRentalOffice.json", RentalOffice.class),
-                JsonUtil.getResourceAsJson("/migration/rental_office/PloiestiRentalOffice.json", RentalOffice.class)
+                JsonUtil.getResourceAsJson("/migration/rental_office/RentalOffice1.json", RentalOffice.class),
+                JsonUtil.getResourceAsJson("/migration/rental_office/RentalOffice2.json", RentalOffice.class)
         );
     }
 
     public static List<Branch> getBranches() {
         return List.of(
-                JsonUtil.getResourceAsJson("/migration/branch/BucurestiBranch.json", Branch.class),
-                JsonUtil.getResourceAsJson("/migration/branch/PloiestiBranch.json", Branch.class)
+                JsonUtil.getResourceAsJson("/migration/branch/Branch1.json", Branch.class),
+                JsonUtil.getResourceAsJson("/migration/branch/Branch2.json", Branch.class)
         );
     }
 
     public static List<Car> getCars() {
         return List.of(
-                JsonUtil.getResourceAsJson("/migration/car/AudiA4.json", Car.class),
-                JsonUtil.getResourceAsJson("/migration/car/VolkswagenGolf.json", Car.class)
+                JsonUtil.getResourceAsJson("/migration/car/Car1.json", Car.class),
+                JsonUtil.getResourceAsJson("/migration/car/Car2.json", Car.class)
         );
     }
 
     public static List<Employee> getEmployees() {
         return List.of(
-                JsonUtil.getResourceAsJson("/migration/employee/AndreiIonescu.json", Employee.class),
-                JsonUtil.getResourceAsJson("/migration/employee/AlexandruOprescu.json", Employee.class),
-                JsonUtil.getResourceAsJson("/migration/employee/IonPopescu.json", Employee.class),
-                JsonUtil.getResourceAsJson("/migration/employee/MariusAlexandrescu.json", Employee.class)
+                JsonUtil.getResourceAsJson("/migration/employee/Employee2.json", Employee.class),
+                JsonUtil.getResourceAsJson("/migration/employee/Employee1.json", Employee.class),
+                JsonUtil.getResourceAsJson("/migration/employee/Employee3.json", Employee.class),
+                JsonUtil.getResourceAsJson("/migration/employee/Employee4.json", Employee.class)
         );
     }
 
